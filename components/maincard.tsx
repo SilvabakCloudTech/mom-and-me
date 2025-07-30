5abc10255062c1c4535711f6a97f5704383aeca6
Muzi Phage <muzi.phage@silvabak.co.za>
2025-07-30 14:14:32 +0200

    Tweaked picture to be a prop

--- components/maincard.tsx
+++ components/maincard.tsx
@@ -1,5 +1,4 @@
 import { Card, CardBody, CardHeader } from "@heroui/card";
-import PhoneIcon from "./phoneicon";
 
 export default function MainCard() {
     return(
@@ -7,7 +6,6 @@ export default function MainCard() {
     <CardHeader className="border bg-transparent text-slate-400 font-mono text-font-bold sm:text-lg md:text-xl lg:text-3xl">MOM & BABY HEALTH IS OUR PRIORITY</CardHeader>
     <CardBody className="bg-transparent font-mono sm:text-medium md:text-lg lg:text-xl xl:text-2xl">
       Welcome to our clinic! We are dedicated to providing the best care for mothers and their babies. Our team of experienced professionals is here to support you every step of the way.
-    <PhoneIcon/>
     </CardBody>
   </Card>
     );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,9 @@
 import {
   Navbar as HeroUINavbar,
   NavbarContent,
-  NavbarBrand,
-  NavbarItem,
 } from "@heroui/navbar";
 import Image from "next/image";
+import bongekapic from "@/public/bongekapic.jpg";
 
 
 export const Navbar = () => {
@@ -12,7 +11,7 @@ export const Navbar = () => {
   return (
     <HeroUINavbar className="bg-pink-200 flex justify-between items-end rounded-lg" >
       <NavbarContent className="flex flex-row justify-start">
-      <Image className="rounded-full object-contain" width={40} height={40} src="/bongekapic.jpg" alt="profile picture"/>
+      <Image className="rounded-full object-contain" width={40} height={40} src={bongekapic} alt="profile picture"/>
           <p className="text-stone-500 font-serif text-xl md:text-2xl font-semibold">MOM & ME CLINIC</p>
       </NavbarContent>
       <NavbarContent className="flex justify-end items-center">
